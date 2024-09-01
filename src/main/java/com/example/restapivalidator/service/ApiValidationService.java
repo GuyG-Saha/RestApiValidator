1fe994cf259a4735f961a452df38390cd6a085d8
guy <guyg7@mail.afeka.ac.il>
2024-09-01 09:47:36 +0300

    remove unused methods

--- src/main/java/com/example/restapivalidator/service/ApiValidationService.java
+++ src/main/java/com/example/restapivalidator/service/ApiValidationService.java
@@ -32,32 +32,4 @@ public class ApiValidationService {
         }
         return true;
     }
-    private boolean validateParams(Map<String, Object> incomingParams, Map<String, Parameter> modelParams) {
-        for (Map.Entry<String, Parameter> entry : modelParams.entrySet()) {
-            String paramName = entry.getKey();
-            Parameter paramModel = entry.getValue();
-            if (Objects.isNull(incomingParams)) {
-                return false;
-            }
-            if (paramModel.isRequired() && !incomingParams.containsKey(paramName)) {
-                return false;
-            }
-            if (paramModel.isRequired() &&
-                    ((LinkedHashMap) incomingParams
-                            .get(paramName))
-                            .get("required").equals(false)) {
-                return false;
-            }
-            // If the parameter is present, check its type
-            Object value = incomingParams.get(paramName);
-            if (value != null && !isTypeMatching(value, paramModel.getType())) {
-                return false;
-            }
-        }
-        return true;
-    }
-    private boolean isTypeMatching(Object value, String expectedType) {
-        return ((LinkedHashMap) value).get("type").equals(expectedType.toLowerCase());
-    }
-
 }
