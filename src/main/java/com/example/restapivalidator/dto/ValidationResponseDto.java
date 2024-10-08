122f8bc80a5c79e6efd9d70ad0cf7e7a64e826d8
guy <guyg7@mail.afeka.ac.il>
2024-10-08 23:17:17 +0300

    refactor ResponseDto to have Map<String, List<String>> of errors instead of just list

--- src/main/java/com/example/restapivalidator/dto/ValidationResponseDto.java
+++ src/main/java/com/example/restapivalidator/dto/ValidationResponseDto.java
@@ -4,11 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
 
 @AllArgsConstructor
 @Data
 public class ValidationResponseDto {
     private String status;
     private String message;
-    private List<String> errors;
+    private Map<String, List<String>> errors;
 }
