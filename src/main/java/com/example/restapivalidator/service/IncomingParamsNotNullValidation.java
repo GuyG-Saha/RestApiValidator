6db4fb450ff6fcb85b9b40a2f356dd7b61a10135
guyg <guy@proxima.co.il>
2025-01-18 23:05:21 +0200

    Elaborate NotNullValidation - checking both fields presence and values being null

--- src/main/java/com/example/restapivalidator/service/IncomingParamsNotNullValidation.java
+++ src/main/java/com/example/restapivalidator/service/IncomingParamsNotNullValidation.java
@@ -4,18 +4,29 @@ import com.example.restapivalidator.dto.ValidationResultDto;
 import com.example.restapivalidator.model.Parameter;
 import org.springframework.stereotype.Component;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @Component
 public class IncomingParamsNotNullValidation implements ValidationRule {
+    private final Map<String, String> faultyParamsDescription = new LinkedHashMap<>();
     @Override
     public ValidationResultDto validate(Map<String, Object> incomingParams, Map<String, Parameter> modelParams) {
         ValidationResultDto resultDto = new ValidationResultDto();
-        if (Objects.isNull(incomingParams))
+        if (Objects.isNull(incomingParams)) {
             resultDto.setValid(false);
+        }
+        else {
+            for (String fieldName : incomingParams.keySet()) {
+                if (modelParams.containsKey(fieldName)) {
+                    if (Objects.isNull(incomingParams.get(fieldName)) && modelParams.get(fieldName).isRequired()) {
+                        resultDto.setValid(false);
+                        faultyParamsDescription.put("ParameterName", fieldName);
+                        faultyParamsDescription.put("RequiredValue", null);
+                        resultDto.getFaultyParams().add(faultyParamsDescription.toString());
+                    }
+                }
+            }
+        }
         return resultDto;
     }
 }
